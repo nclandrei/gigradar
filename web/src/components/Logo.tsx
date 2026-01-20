3306f4db850ed63ceb8719582dde2f5aea210e3a
Andrei-Mihai Nicolae <andrei@nicolaeandrei.com>
2026-01-20 11:44:34 +0200

    Fix logo hover underline cutting through descenders

--- web/src/components/Logo.tsx
+++ web/src/components/Logo.tsx
@@ -13,7 +13,7 @@ export function Logo() {
       />
       <span className="relative text-3xl md:text-5xl font-bold tracking-tight leading-none">
         Cultură la plic
-        <span className="absolute -bottom-1 left-0 w-0 h-3 bg-culture -rotate-1 group-hover:w-full transition-all duration-300" />
+        <span className="absolute -bottom-3 left-0 w-0 h-3 bg-culture -rotate-1 group-hover:w-full transition-all duration-300" />
       </span>
     </Link>
   );
